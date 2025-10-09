5d4f0037aeaf4ca146d6c982f65834f04f17d232
Andrew Grant <atgrant11@gmail.com>
2025-10-09 17:22:33 -0500

    Add files via upload

---
+++ me.js
@@ -0,0 +1,13 @@
+// Andrew Grant 10/09/2025
+
+let fname = "Andrew Grant";
+let favColor = "red";
+let favHobby = "gaming";
+let favFood = "chinese food";
+let favAnimal = "raccoons";
+
+console.log("My name is " + fname);
+console.log("My favorite color is " + favColor);
+console.log("My favorite hobby is " + favHobby);
+console.log("My favorite food is " + favFood);
+console.log("My favorite animal is " + favAnimal);
